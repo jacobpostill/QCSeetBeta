60e80710c289ab2e6eb06b9b6f3eb27da88c592a
jacobpostill <122237133+jacobpostill@users.noreply.github.com>
2023-07-27 14:51:55 -0400

    Create Seam.jsx

---
+++ client/src/createReport/canLine/Seam.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+
+const Seam = () => {
+  const [seamSubmitted, setSeamSubmitted] = useState(false);
+  const [isInfinity, setIsInfinity] = useState(false);
+  const [explanation, setExplanation] = useState('');
+
+  const handleSeamSubmit = () => {
+    setSeamSubmitted(!seamSubmitted);
+  };
+
+  const handleCheckboxChange = (e) => {
+    const { name, checked } = e.target;
+    if (name === 'isInfinity') {
+      setIsInfinity(checked);
+    }
+  };
+
+  return (
+    <div>
+      <h3>Seam Submitted</h3>
+      <input
+        type="checkbox"
+        checked={seamSubmitted}
+        onChange={handleSeamSubmit}
+      />
+      {seamSubmitted && (
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              name="isInfinity"
+              checked={isInfinity}
+              onChange={handleCheckboxChange}
+            />
+            Infinity
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              name="isPaper"
+              checked={!isInfinity}
+              onChange={handleCheckboxChange}
+            />
+            Paper
+          </label>
+          {!isInfinity && (
+            <div>
+              <p>If paper, please explain why you did not use infinity:</p>
+              <input
+                type="text"
+                value={explanation}
+                onChange={(e) => setExplanation(e.target.value)}
+              />
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Seam;
+
